f1446e2a24901787aa2b94e25c8fc73fb9963c22
MICHEL Adrien <privateadrienps@gmail.com>
2022-05-23 22:01:33 +0200

    Skill fragments : typo.

--- app/src/main/java/com/adrienmandroid/composecv/data/dataSource/impl/SkillDataImpl.kt
+++ app/src/main/java/com/adrienmandroid/composecv/data/dataSource/impl/SkillDataImpl.kt
@@ -10,7 +10,7 @@ class SkillDataImpl : SkillData {
             La technologie qui me plait le plus.
             Je suis très content d'avoir quitté le développement Peoplesoft pour passer sur Android.
             Plus je travaille dessus, plus je vois que la route et longue, et continuera à s'étendre avec toutes les évolutions à venir.
-            Je suis impatient de voir ce que je vais apprendre demain.
+            Je suis impatient de voir ce que je vais apprendre sur Android demain.
         """),
         Skill("Kotlin", 0.8f, """
             Une des raisons pour laquel Android est si plaisant à travailler est Kotlin.
@@ -18,18 +18,18 @@ class SkillDataImpl : SkillData {
             Avec ce langage, y a toujours quelque chose à apprendre pour optimiser son code. En efficacité et en lisibilité.    
         """),
         Skill("Structure MVVM", 0.7f, """
-            Une des difficulté les plus redoutées dans l'entrée du monde du travail est de travailler sur des projets déjà existants et incommensurément complexes.
+            Une des difficulté les plus redoutées dans l'entré du monde du travail est de travailler sur des projets déjà existants et incommensurément complexes.
             Les structures de développements aident à soulager cette diffulté.
-            C'est pour cela que je me tiens au courant des dernières évolutions et questionne toujours pour optimiser et adapter l'exisstant.
+            C'est pour cela que je me tiens au courant des dernières évolutions et me questionne toujours pour optimiser et adapter l'exisstant.
         """),
         Skill("Test Driven Development", 0.5f, """
-            Cela demande d'apprendre de nombreux outils de tests et de structure son code pour ces derniers.
+            Cela demande d'apprendre de nombreux outils de tests et de structurer son code pour ces derniers.
             Cela en vaut clairement le coup.
             Avoir son code validé tout au long de son développement est non seulement rassurant, mais surtout très satisfaisant. 
         """),
         Skill("Hilt", 0.7f, """
             Une révolution du développement que j'adore.
-            L'injection de dépendance rend le code beaucoup plus maintenable, mais il y a bien un endroit o cela pique : quand ces dépendances doivent être injectées.
+            L'injection de dépendance rend le code beaucoups plus maintenable, mais il y a bien un endroit où cela pique : quand ces dépendances doivent être injectées.
             Avec Hilt pas de soucis, ces injections sont gérées de façon très lisible."""),
         Skill("Jetpack UI", 0.8f, """
             Constraint layout, Navigation drawers, Tabs, Recycler views.
@@ -37,9 +37,9 @@ class SkillDataImpl : SkillData {
         """),
         Skill("Jetpack compose", 0.7f, """
             Le dernier bébé de Google.
-            Intrigué et curieux, j'ai évidemment travaillé sur ce projet personnel pour apprendre à l'utiliser.
+            Intrigué et curieux, j'ai évidemment choisit de faire ce projet personnel avec Compose pour apprendre à l'utiliser.
             J'ai été un peu déçu.
-            Compose est bien mieux que l'xml. Mais ne m'a pas décoiffé comme l'injection de dépendance et hilt l'ont fait.
+            Compose est bien mieux que l'xml. Mais ne m'a pas décoiffé comme l'injection de dépendance et Hilt l'ont fait.
         """),
         Skill("English", 0.9f, """
             With my multiple travels in American speaking countries (and the time spent browsing internet), I am fluent in English.
@@ -48,7 +48,7 @@ class SkillDataImpl : SkillData {
         Skill("German", 0.5f, """
             Ich habe Deutch in Schule gelernt. Im 2022, Ich möchte noch ein mal Deutch lernen.
             Ich sprechen ein bissien deutch.
-        """.trimIndent())
+        """)
         //Skill("Super skill3", 0.7f, "Texte skill 3"),
 
     )
