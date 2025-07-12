6f20969593d5b92ca190d57640fc6dd5a05fe9c6
Adrien <developpeuradrien@gmail.com>
2025-07-12 19:37:23 +0200

    Creating Experience ui module

---
+++ feature/experience/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.convention.library)
+    alias(libs.plugins.convention.compose.library)
+    alias(libs.plugins.convention.hilt.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.adrienmandroid.composecv.feature.experience.ui"
+
+}
+
+dependencies {
+    implementation(project(":core:data"))
+    implementation(project(":core:ui"))
+    debugImplementation(project(":core:test"))
+}
