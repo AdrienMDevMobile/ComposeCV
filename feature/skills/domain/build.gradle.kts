5ca4e6f915d9557f3d31707c4275cfbab7b5024b
Adrien <developpeuradrien@gmail.com>
2025-07-16 08:29:33 +0200

    Create Skills domain

---
+++ feature/skills/domain/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.convention.library)
+    alias(libs.plugins.convention.compose.library)
+    alias(libs.plugins.convention.hilt.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.adrienmandroid.composecv.feature.skills.domain"
+}
+
+dependencies {
+    implementation(project(":core:data"))
+    implementation(project(":core:ui"))
+}
