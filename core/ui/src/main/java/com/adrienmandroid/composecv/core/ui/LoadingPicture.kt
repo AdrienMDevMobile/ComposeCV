4140352f912e17ca7d05597628191b8c4b9fce65
Adrien <developpeuradrien@gmail.com>
2025-07-17 19:38:27 +0200

    Moving Content loader to a core class

---
+++ core/ui/src/main/java/com/adrienmandroid/composecv/core/ui/LoadingPicture.kt
@@ -0,0 +1,66 @@
+package com.adrienmandroid.composecv.core.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun LoadingPicture(
+    pictureUrl: String,
+    modifier: Modifier = Modifier,
+    placeholderPainter: Painter = painterResource(R.drawable.core_placeholder),
+    contentDescription: String = "@null",
+) {
+
+    var isLoading by remember { mutableStateOf(true) }
+    var isError by remember { mutableStateOf(false) }
+    val imageLoader = rememberAsyncImagePainter(
+        model = pictureUrl,
+        onState = { state ->
+            isLoading = state is AsyncImagePainter.State.Loading
+            isError = state is AsyncImagePainter.State.Error
+        },
+    )
+    val isLocalInspection = LocalInspectionMode.current
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        if (isLoading) {
+            // Display a progress bar while loading
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(80.dp),
+                color = MaterialTheme.colors.secondary,
+            )
+        }
+
+        Image(
+            modifier = modifier,
+            contentScale = ContentScale.Crop,
+            painter = if (isError.not() && !isLocalInspection) {
+                imageLoader
+            } else {
+                placeholderPainter
+            },
+            contentDescription = contentDescription,
+        )
+    }
+}
--- feature/welcome/ui/src/main/java/com/adrienmandroid/composecv/feature/welcome/ui/element/WelcomeBackgroundPicture.kt
+++ feature/welcome/ui/src/main/java/com/adrienmandroid/composecv/feature/welcome/ui/element/WelcomeBackgroundPicture.kt
@@ -1,69 +1,18 @@
 package com.adrienmandroid.composecv.feature.welcome.ui.element
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import com.adrienmandroid.composecv.feature.welcome.ui.R
+import com.adrienmandroid.composecv.core.ui.LoadingPicture
 
 @Composable
 fun WelcomeBackgroundPicture(welcomeImageUrl: String) {
-
-    var isLoading by remember { mutableStateOf(true) }
-    var isError by remember { mutableStateOf(false) }
-    val imageLoader = rememberAsyncImagePainter(
-        model = welcomeImageUrl,
-        onState = { state ->
-            isLoading = state is AsyncImagePainter.State.Loading
-            isError = state is AsyncImagePainter.State.Error
-        },
-    )
-    val isLocalInspection = LocalInspectionMode.current
-    Box(
+    LoadingPicture(
+        pictureUrl = welcomeImageUrl,
         modifier = Modifier
             .fillMaxWidth()
             .height(250.dp),
-        contentAlignment = Alignment.Center,
-    ) {
-        if (isLoading) {
-            // Display a progress bar while loading
-            CircularProgressIndicator(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .size(80.dp),
-                color = MaterialTheme.colors.secondary,
-            )
-        }
-
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(250.dp),
-            contentScale = ContentScale.Crop,
-            painter = if (isError.not() && !isLocalInspection) {
-                imageLoader
-            } else {
-                //TODO real placeholder
-                painterResource(R.drawable.core_placeholder)
-            },
-            // TODO
-            contentDescription = "Background",
-        )
-    }
+    )
 }
