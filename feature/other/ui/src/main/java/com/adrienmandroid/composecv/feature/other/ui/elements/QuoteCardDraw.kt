e3bd34481fecef913b08f699c988690c2d3c2408
Adrien <developpeuradrien@gmail.com>
2025-08-07 10:49:07 +0200

    Quote pictures from internet

--- feature/other/ui/src/main/java/com/adrienmandroid/composecv/feature/other/ui/elements/QuoteCardDraw.kt
+++ feature/other/ui/src/main/java/com/adrienmandroid/composecv/feature/other/ui/elements/QuoteCardDraw.kt
@@ -7,14 +7,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -23,12 +30,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import com.adrienmandroid.composecv.core.ui.theme.ComposeCVTheme
 import com.adrienmandroid.composecv.core.ui.theme.onQuoteBackground
 import com.adrienmandroid.composecv.core.ui.theme.quoteBackground
+import com.adrienmandroid.composecv.feature.other.domain.model.Quote
 import com.adrienmandroid.composecv.feature.other.ui.R
 import com.adrienmandroid.composecv.feature.other.ui.preview.data.QuoteIndexedPreviewParameterProvider
-import com.adrienmandroid.composecv.feature.other.domain.model.Quote
 
 @Composable
 fun QuoteCardDraw(quote: Quote, position: Int) {
@@ -59,60 +68,71 @@ fun QuoteCardDraw(quote: Quote, position: Int) {
             .padding(10.dp, 10.dp), backgroundColor = MaterialTheme.colors.quoteBackground,
         elevation = 5.dp
     ) {
-        ConstraintLayout {
-            val (text, image, author, quoteTop, quoteBottom) = createRefs()
+        var isLoading by remember { mutableStateOf(true) }
+        var isError by remember { mutableStateOf(false) }
+        val imageLoader = rememberAsyncImagePainter(
+            model = quote.imageUrl,
+            onState = { state ->
+                isLoading = state is AsyncImagePainter.State.Loading
+                isError = state is AsyncImagePainter.State.Error
+            },
+        )
+        val isLocalInspection = LocalInspectionMode.current
 
-            Icon(
-                painter = painterResource(id = R.drawable.ic_quote),
-                contentDescription = "quote",
-                modifier = Modifier
-                    .rotate(180F)
-                    .size(quotationMarkSize)
-                    .constrainAs(quoteTop) {
-                        start.linkTo(text.start, margin = (-12).dp)
-                        bottom.linkTo(text.top, margin = (-28).dp)
-                    },
-                tint = Color.Gray
-            )
-            Icon(
-                painter = painterResource(id = R.drawable.ic_quote),
-                contentDescription = "quote",
-                modifier = Modifier
-                    .size(quotationMarkSize)
-                    .constrainAs(quoteBottom) {
-                        end.linkTo(text.end, margin = (-12).dp)
-                        top.linkTo(text.bottom, margin = (-28).dp)
-                    },
-                tint = Color.Gray
-            )
-            Text(
-                text = quote.text,
-                style = textStyleQuote,
-                modifier = Modifier.constrainAs(text) {
-                    if (textLeft) {
-                        start.linkTo(parent.start, margin = marginStart)
-                        end.linkTo(image.start, margin = marginBetween)
-                    } else {
-                        start.linkTo(image.end, margin = marginBetween)
-                        end.linkTo(parent.end, margin = marginEnd)
+        ConstraintLayout {
+            val (text, image, author, quoteTop, quoteBottom, loader) = createRefs()
+
+            if (!isLoading && !isError) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_quote),
+                    contentDescription = "quote",
+                    modifier = Modifier
+                        .rotate(180F)
+                        .size(quotationMarkSize)
+                        .constrainAs(quoteTop) {
+                            start.linkTo(text.start, margin = (-12).dp)
+                            bottom.linkTo(text.top, margin = (-28).dp)
+                        },
+                    tint = Color.Gray
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_quote),
+                    contentDescription = "quote",
+                    modifier = Modifier
+                        .size(quotationMarkSize)
+                        .constrainAs(quoteBottom) {
+                            end.linkTo(text.end, margin = (-12).dp)
+                            top.linkTo(text.bottom, margin = (-28).dp)
+                        },
+                    tint = Color.Gray
+                )
+                Text(
+                    text = quote.text,
+                    style = textStyleQuote,
+                    modifier = Modifier.constrainAs(text) {
+                        if (textLeft) {
+                            start.linkTo(parent.start, margin = marginStart)
+                            end.linkTo(image.start, margin = marginBetween)
+                        } else {
+                            start.linkTo(image.end, margin = marginBetween)
+                            end.linkTo(parent.end, margin = marginEnd)
+                        }
+                        top.linkTo(parent.top, margin = marginTop)
+                        bottom.linkTo(author.top)
+                        width = Dimension.fillToConstraints
                     }
-                    top.linkTo(parent.top, margin = marginTop)
-                    bottom.linkTo(author.top)
-                    width = Dimension.fillToConstraints
-                }
-            )
-            Text(
-                text = quote.author,
-                style = textStyleAuthor,
-                modifier = Modifier.constrainAs(author) {
-                    top.linkTo(text.bottom)
-                    bottom.linkTo(parent.bottom, margin = 16.dp)
-                    end.linkTo(text.end)
-                }
-            )
+                )
+                Text(
+                    text = quote.author,
+                    style = textStyleAuthor,
+                    modifier = Modifier.constrainAs(author) {
+                        top.linkTo(text.bottom)
+                        bottom.linkTo(parent.bottom, margin = 16.dp)
+                        end.linkTo(text.end)
+                    }
+                )
+            }
             Image(
-                painter = painterResource(id = quote.image),
-                contentDescription = quote.author,
                 modifier = Modifier
                     .constrainAs(image) {
                         if (!textLeft) {
@@ -125,8 +145,26 @@ fun QuoteCardDraw(quote: Quote, position: Int) {
                         top.linkTo(parent.top, margin = marginTop)
                         bottom.linkTo(parent.bottom, margin = marginBottom)
                         height = Dimension.fillToConstraints
-                    }
+                    },
+                contentScale = ContentScale.Crop,
+                painter = if (isError.not() && !isLocalInspection) {
+                    imageLoader
+                } else {
+                    painterResource(com.adrienmandroid.composecv.core.ui.R.drawable.core_placeholder)
+                },
+                contentDescription = quote.author,
             )
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.constrainAs(loader){
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        top.linkTo(parent.top, margin = 4.dp)
+                        bottom.linkTo(parent.bottom, margin = 4.dp)
+                    },
+                    color = MaterialTheme.colors.secondary,
+                )
+            }
         }
     }
 }
--- feature/other/ui/src/main/java/com/adrienmandroid/composecv/feature/other/ui/preview/data/QuotesPreviewParameterProvider.kt
+++ feature/other/ui/src/main/java/com/adrienmandroid/composecv/feature/other/ui/preview/data/QuotesPreviewParameterProvider.kt
@@ -1,10 +1,10 @@
 package com.adrienmandroid.composecv.feature.other.ui.preview.data
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import com.adrienmandroid.composecv.core.test.R
 import com.adrienmandroid.composecv.feature.other.domain.model.Quote
 
-class QuoteIndexedPreviewParameterProvider : PreviewParameterProvider<Pair<Int, com.adrienmandroid.composecv.feature.other.domain.model.Quote>> {
+class QuoteIndexedPreviewParameterProvider :
+    PreviewParameterProvider<Pair<Int, Quote>> {
     override val values: Sequence<Pair<Int, Quote>>
         get() = QuotePreviewParameterData.quotes.asSequence()
             .mapIndexed { index, quote -> index to quote }
@@ -15,12 +15,12 @@ object QuotePreviewParameterData {
         Quote(
             "Lorem ipsum dolor sit amet.",
             "Lorem ipsum",
-            R.drawable.img_test,
+            "",
         ),
         Quote(
             "Lorem ipsum dolor sit amet.",
             "Lorem ipsum",
-            R.drawable.img_test,
+            "",
         ),
     )
 }
