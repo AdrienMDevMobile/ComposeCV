1f3440c72070418a88405e5d6e9867df9d84cff2
Adrien <developpeuradrien@gmail.com>
2025-11-04 10:26:56 +0100

    Welcome local room provider part 1

---
+++ feature/welcome/data/src/main/java/com/adrienmandroid/composecv/feature/data/converter/WelcomeLocalConverterExt.kt
@@ -0,0 +1,65 @@
+package com.adrienmandroid.composecv.feature.data.converter
+
+import com.adrienmandroid.composecv.data.local.toJavaDate
+import com.adrienmandroid.composecv.data.local.toRoomString
+import com.adrienmandroid.composecv.feature.data.converter.WelcomeEntityTypes.BIRTHDAY_TYPE
+import com.adrienmandroid.composecv.feature.data.converter.WelcomeEntityTypes.IMAGE_TYPE
+import com.adrienmandroid.composecv.feature.data.converter.WelcomeEntityTypes.KEYWORDS_TYPE
+import com.adrienmandroid.composecv.feature.data.converter.WelcomeEntityTypes.QUOTE_TYPE
+import com.adrienmandroid.composecv.feature.data.converter.WelcomeEntityTypes.TEXT_TYPE
+import com.adrienmandroid.composecv.feature.data.local.WelcomeElementEntity
+import com.adrienmandroid.composecv.feature.welcome.domain.model.Keyword
+import com.adrienmandroid.composecv.feature.welcome.domain.model.WelcomeBodyElement
+
+fun WelcomeBodyElement.toLocalData() = when (this) {
+    is WelcomeBodyElement.BirthdayText -> WelcomeElementEntity(
+        type = BIRTHDAY_TYPE,
+        value = value.toRoomString()
+    )
+
+    is WelcomeBodyElement.WelcomeImage -> WelcomeElementEntity(
+        type = IMAGE_TYPE,
+        //TODO
+    )
+
+    is WelcomeBodyElement.WelcomeKeywordList -> WelcomeElementEntity(
+        type = KEYWORDS_TYPE,
+        value = fromListString(keywords)
+    )
+
+    is WelcomeBodyElement.WelcomeQuote -> WelcomeElementEntity(
+        type = QUOTE_TYPE,
+        value = quote
+    )
+
+    is WelcomeBodyElement.WelcomeText -> WelcomeElementEntity(
+        type = TEXT_TYPE,
+        value = value,
+        //TODO typography et clickable
+    )
+}
+
+fun WelcomeElementEntity.toDomain() = when (type) {
+    BIRTHDAY_TYPE -> if (value != null) WelcomeBodyElement.BirthdayText(value.toJavaDate()) else null
+    IMAGE_TYPE -> TODO()
+    KEYWORDS_TYPE -> TODO()
+    QUOTE_TYPE -> TODO()
+    TEXT_TYPE -> TODO()
+    else -> null
+}
+
+fun fromListString(list: List<Keyword>): String {
+    return list.joinToString(",") { it.toString() }
+}
+
+fun toListString(data: String): List<Keyword> {
+    return listOf(*data.split(",").map { Keyword(it) }.toTypedArray())
+}
+
+private object WelcomeEntityTypes {
+    const val BIRTHDAY_TYPE = "BIRTHDAY"
+    const val IMAGE_TYPE = "IMAGE"
+    const val KEYWORDS_TYPE = "KEYWORDS"
+    const val QUOTE_TYPE = "QUOTE"
+    const val TEXT_TYPE = "TEXT"
+}
