4a63b4c059ca65d0a05b3b3175904e9d697f7be1
Adrien <developpeuradrien@gmail.com>
2025-12-10 12:22:06 +0100

    Edge to edge

--- app/src/main/java/com/adrienmandroid/composecv/MainActivity.kt
+++ app/src/main/java/com/adrienmandroid/composecv/MainActivity.kt
@@ -2,9 +2,12 @@ package com.adrienmandroid.composecv
 
 import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material.ExperimentalMaterialApi
 import com.adrienmandroid.composecv.core.ui.theme.ComposeCVTheme
 import com.adrienmandroid.composecv.ui.MainScreen
@@ -16,6 +19,10 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            //navigationBarStyle = SystemBarStyle.auto(DefaultLightScrim, DefautDarkScrim)
+        )
         requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
         setContent {
             ComposeCVTheme {
